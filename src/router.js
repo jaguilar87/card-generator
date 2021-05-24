e325a4efd6558247d1458a04bc11f5380d0df594
Jordi Aguilar <jaguilargorina@newrelic.com>
2021-05-24 19:59:41 +0200

    feat: Remove lead hounds

--- src/router.js
+++ src/router.js
@@ -2,14 +2,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import NetEA from './cards/netea/index.vue';
-import LeadHounds from './cards/lead-hounds/index.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     getRoute('netea', NetEA),
-    getRoute('lead-hounds', LeadHounds),
     {
       path: '/',
       name: 'home',
